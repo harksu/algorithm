7b24428abe3c342592a93dab3ef2d2e23ec16d02
harksu <harksulim@naver.com>
2022-07-17 18:09:00 +0900

    배열시작

--- BaekJoon/배열/최소,최대.py
+++ BaekJoon/배열/최소,최대.py
@@ -1,11 +1,5 @@
 n = int(input())
-num1,num2,num3,num4,num5 = map(int,input().split())
-list = []
-list.append(num1)
-list.append(num2)
-list.append(num3)
-list.append(num4)
-list.append(num5)
-list.sort()
-print('{0} {1}'.format(list[0],list[4]))
+numbers = list(map(int,input().split()))
+numbers.sort()
+print(min(numbers),max(numbers))
 
